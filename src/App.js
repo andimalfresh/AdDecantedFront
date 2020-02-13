13745f8786c8037eb637e09ced81ea4fed62a780
Andrew Pedersen <39564158+andimalfresh@users.noreply.github.com>
2020-02-13 09:29:51 -0700

    Fix

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ class App extends Component {
           />
           <Switch>
             <Route exact path="/" render={() => <Landing />} />
-            <Route path="/survey" render={() => <Survey />} />
-            <Route path="/exportData" render={() => <ExportData customerReturn={this.state.customerReturn} />} />
+            <Route exact path="/survey" render={() => <Survey />} />
+            <Route exact path="/exportData" render={() => <ExportData customerReturn={this.state.customerReturn} />} />
           </Switch>
         </div>
       </div>
